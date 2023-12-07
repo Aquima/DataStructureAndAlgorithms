96c4dcdba44224d0a5412506968f1fd7063a9b52
Raul S. Quispe Mamani <raul.quispe@live.com>
2023-12-07 15:47:54 -0500

    add files

--- DataStructureAndAlgorithms/Sort/BubbleSort.swift
+++ DataStructureAndAlgorithms/Sort/BubbleSort.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+struct BubbleSort {
+
+    /// Removes and returns the last element of the collection.
+    ///
+    /// In Bubble Sort algorithm,
+    /// traverse from left and compare adjacent elements and the higher one is placed at right side.
+    /// In this way, the largest element is moved to the rightmost end at first.
+    /// This process is then continued to find the second largest and place it and so on until the data is sorted.
+    ///
+    /// - Parameter array: The collection it has more than two elements.
+    /// - Returns: The new sorted array
+    ///
+    /// - Complexity: O(n^2) where *n* is the length of the sequence.
+    func sort(_ array: inout [Int]) -> [Int] {
+        guard array.count >= 2 else {
+            return array
+        }
+        let allValues = array.count
+        let length = allValues - 1
+        for end in (0..<allValues).reversed() {
+            var swapped = false
+            //This loop performs a single pass; it compares adjacent values and swaps them if needed.
+            for currentIndex in 0..<end {
+              if array[currentIndex] > array[currentIndex+1] {
+                  array.swapAt(currentIndex, currentIndex + 1)
+                  swapped = true
+              }
+            }
+            //if no values were swapped this pass, the collection must be sorted, and you can exit early.
+            if !swapped {
+                return array
+            }
+          }
+        return array
+    }
+}
--- DataStructureAndAlgorithmsTests/Sort/BubbleSortTests.swift
+++ DataStructureAndAlgorithmsTests/Sort/BubbleSortTests.swift
@@ -9,31 +9,27 @@ import XCTest
 @testable import DataStructureAndAlgorithms
 
 final class BubbleSortTests: XCTestCase {
-
-
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testSort() throws {
+        var sut = sut()
+        let bubbleSort = BubbleSort()
+        XCTAssertTrue(bubbleSort.sort(&sut) == [ 1, 2, 4, 5, 6, 9])
     }
-
+    
+    func testMajorThanTwo() throws {
+        let sut = sut()
+        XCTAssertTrue(sut.count > 1)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
-    func testMajorThanTwo() throws {
-        let sut = sut()
-        let bubleSort = BubbleSort()
-    }
     // MARK: HELPERS
     func sut() -> [Int] {
-        return [4, 2, 5, 6]
+        return [4, 2, 5, 6, 1, 9]
     }
 
 }
